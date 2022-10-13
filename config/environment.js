3967b468f99418558e422136a5d1e574d1625ba1
Chris Manson <chris@manson.ie>
2022-10-13 14:43:24 +0100

    reset to 3.28

--- config/environment.js
+++ config/environment.js
@@ -5,11 +5,15 @@ module.exports = function (environment) {
     modulePrefix: 'emberfest-watchlist',
     environment,
     rootURL: '/',
-    locationType: 'history',
+    locationType: 'auto',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. EMBER_NATIVE_DECORATOR_SUPPORT: true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false,
       },
     },
 
